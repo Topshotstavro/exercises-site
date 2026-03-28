4f5a33bc885a665cb2d5a036994348ea04bec20c
Topshotstavro <79659516+Topshotstavro@users.noreply.github.com>
2026-03-27 20:14:18 -0400

    Update loader.js

--- codereuse/library/loader.js
+++ codereuse/library/loader.js
@@ -1,10 +1,10 @@
-fetch("/library/navbar.html")
+fetch("library/navbar.html")
   .then(r => r.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
   });
 
-fetch("/library/footer.html")
+fetch("library/footer.html")
   .then(r => r.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
