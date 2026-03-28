23708c19bc8e526059088093a409dca2b379b50a
Topshotstavro <79659516+Topshotstavro@users.noreply.github.com>
2026-03-27 20:18:55 -0400

    Update loader.js

--- codereuse/library/loader.js
+++ codereuse/library/loader.js
@@ -1,10 +1,16 @@
-fetch("library/navbar.html")
+let prefix = "";
+
+if (window.location.pathname.includes("/yucky-things/")) {
+  prefix = "../";
+}
+
+fetch(prefix + "library/navbar.html")
   .then(r => r.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
   });
 
-fetch("library/footer.html")
+fetch(prefix + "library/footer.html")
   .then(r => r.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
